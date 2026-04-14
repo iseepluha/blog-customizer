c7e61674b48d7eee8c9373baed11251d31fdd8db
iseepluha <qwezxcabuzer@gmail.com>
2026-04-14 22:22:19 +0300

    refactor: move App component to its own file

--- src/index.tsx
+++ src/index.tsx
@@ -1,53 +1,9 @@
 import { createRoot } from 'react-dom/client';
-import { StrictMode, CSSProperties, useState } from 'react';
-import clsx from 'clsx';
-
-import { Article } from './components/article/Article';
-import { ArticleParamsForm } from './components/article-params-form/ArticleParamsForm';
-import {
-	ArticleStateType,
-	defaultArticleState,
-} from './constants/articleProps';
-
-import './styles/index.scss';
-import styles from './styles/index.module.scss';
+import { StrictMode } from 'react';
+import { App } from './App';
 
 const domNode = document.getElementById('root') as HTMLDivElement;
 const root = createRoot(domNode);
-
-const App = () => {
-	const [articleState, setArticleState] =
-		useState<ArticleStateType>(defaultArticleState);
-
-	const handleApply = (newState: ArticleStateType) => {
-		setArticleState(newState);
-	};
-
-	const handleReset = () => {
-		setArticleState(defaultArticleState);
-	};
-	return (
-		<main
-			className={clsx(styles.main)}
-			style={
-				{
-					'--font-family': articleState.fontFamilyOption.value,
-					'--font-size': articleState.fontSizeOption.value,
-					'--font-color': articleState.fontColor.value,
-					'--container-width': articleState.contentWidth.value,
-					'--bg-color': articleState.backgroundColor.value,
-				} as CSSProperties
-			}>
-			<ArticleParamsForm
-				onApply={handleApply}
-				onReset={handleReset}
-				initialValues={articleState}
-			/>
-			<Article />
-		</main>
-	);
-};
-
 root.render(
 	<StrictMode>
 		<App />
